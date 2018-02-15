29e8d40c996a5e098b6d8c6049e64b4b887a26ad
khaliddabjan <khalid.dabjan@gmail.com>
2018-02-15 10:29:57 +0400

    Traversable Queue

---
+++ src/Queues/TraversableQueue.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaliddabjan
+ * Date: 2/15/18
+ * Time: 10:06 AM
+ */
+
+namespace src\Queues;
+
+
+class TraversableQueue extends Queue implements \Iterator
+{
+    private $position = 0;
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->queue[$this->position];
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return isset($this->queue[$this->position]);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+}
---
+++ tests/TraversableQueueTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaliddabjan
+ * Date: 2/15/18
+ * Time: 10:08 AM
+ */
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use src\Queues\TraversableQueue;
+
+class TraversableQueueTest extends TestCase
+{
+    protected $traversableQueue;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->traversableQueue = new TraversableQueue();
+    }
+
+    /** @test */
+    public function it_can_be_traversed()
+    {
+        $this->traversableQueue->enqueue(0);
+        $this->traversableQueue->enqueue(1);
+        $this->traversableQueue->enqueue(2);
+        $this->traversableQueue->enqueue(3);
+        foreach ($this->traversableQueue as $index => $item) {
+            $this->assertEquals($item, $index);
+        }
+    }
+}
